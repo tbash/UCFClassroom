b74bf089d1f89a8190d171d8547d8b7df525adab
teebash <tbash@knights.ucf.edu>
2015-09-07 20:30:55 -0400

    dev branch created

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150907232258) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "description", limit: 65535
+    t.date     "add_drop_by"
+    t.boolean  "available"
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+  end
 
 end
