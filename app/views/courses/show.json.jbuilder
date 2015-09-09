b8298dc8ff7963655b100648df83969fed80dfb9
teebash <tbash@knights.ucf.edu>
2015-09-09 08:41:51 -0400

    :memo: README updated with changes, created User model

--- app/views/courses/show.json.jbuilder
+++ app/views/courses/show.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! @course, :id, :name, :description, :add_drop_by, :available, :created_at, :updated_at
+json.extract! @course, :id, :name, :instructor_id, :session_days, :session_time, :description, :start_date, :end_date, :available, :video_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150907232258) do
+ActiveRecord::Schema.define(version: 20150909115847) do
 
   create_table "courses", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.text     "description", limit: 65535
-    t.date     "add_drop_by"
+    t.string   "name",             limit: 255
+    t.integer  "instructor_id",    limit: 4
+    t.string   "session_days",     limit: 255
+    t.time     "session_time"
+    t.text     "description",      limit: 65535
+    t.date     "start_date"
+    t.date     "end_date"
     t.boolean  "available"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.string   "video_id",         limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.time     "session_time_out"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
