9d13517239259d4ee3dbe8aa656f64011ca6270f
Timothy Ashley <tbashley@Timothys-MacBook-Air.local>
2015-09-25 23:36:30 -0400

    Pushing up quick README changes and adding redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,14 @@
-$redis = Redis.new 
+$redis = Redis.new
+
+heartbeat_thread = Thread.new do
+  while true
+    $redis.publish("heartbeat","thump")
+    sleep 30.seconds
+  end
+end
+
+at_exit do
+  # not sure this is needed, but just in case
+  heartbeat_thread.kill
+  $redis.quit
+end
