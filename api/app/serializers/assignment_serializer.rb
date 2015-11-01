1c72b1a42a9ea1729c89dc169bf1fb52a8890820
T. Brandon Ashley <tbash@knights.ucf.edu>
2015-11-01 11:22:45 -0500

    Adds associations to serializers

--- api/app/serializers/assignment_serializer.rb
+++ api/app/serializers/assignment_serializer.rb
@@ -1,3 +1,5 @@
 class AssignmentSerializer < ActiveModel::Serializer
   attributes :id, :title, :type, :due, :grade, :course_id, :student_id
+  has_one :course
+  has_one :student
 end
--- api/app/serializers/course_serializer.rb
+++ api/app/serializers/course_serializer.rb
@@ -1,3 +1,5 @@
 class CourseSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :instructor_id
+  has_many :slides
+  has_many :assignments
 end
