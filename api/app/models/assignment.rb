28d85db5a29e1a9672d25d1216e8ae16b01e224a
T. Brandon Ashley <tbash@knights.ucf.edu>
2015-11-29 01:55:04 -0500

    accepts nested attr in assignments model

--- api/app/models/assignment.rb
+++ api/app/models/assignment.rb
@@ -2,6 +2,7 @@ class Assignment < ActiveRecord::Base
   belongs_to :course
   belongs_to :user
   has_many :problems
+  accepts_nested_attributes_for :problems
 
   enum assignment_type: [:practice, :base, :quiz, :midterm, :final]
 end
